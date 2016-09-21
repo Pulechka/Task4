0ceac103a8ae2d01892274803ab1a2ed6e774829
AV <vasilenkoanna94@gmail.com>
2016-09-21 20:19:24 +0300

    custom sort demo

---
+++ CustomSort/ArrayWorker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSort
+{
+    public static class ArrayWorker
+    { 
+
+        public static void SortArray<T>(T[] array, CompareTwoObjects<T> compare)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length-1; j++)
+                {
+                    if (compare(array[j],array[j + 1]) > 0)
+                    {
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- CustomSort/Program.cs
+++ CustomSort/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+public delegate int CompareTwoObjects<T>(T obj1, T obj2);
 
 namespace CustomSort
 {
@@ -10,6 +8,42 @@ namespace CustomSort
     {
         static void Main(string[] args)
         {
+            string[] arrayString = new string[] { "зима","весна","лето","осень","ёж","модернизация","Осень","палата","ток","обвал","кот",};
+            ArrayWorker.PrintArray<string>(arrayString);
+
+            CompareTwoObjects<string> compareString = CompareStringByLength;
+            ArrayWorker.SortArray<string>(arrayString, compareString);
+            ArrayWorker.PrintArray<string>(arrayString);
+
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            int[] arrayInt = new int[] { 98, 34,8,-3, 873, -23, 54, 293, 0, -87, 395};
+            ArrayWorker.PrintArray<int>(arrayInt);
+
+            CompareTwoObjects<int> compareInt = CompareInt;
+            ArrayWorker.SortArray<int>(arrayInt, compareInt);
+            ArrayWorker.PrintArray<int>(arrayInt);
+        }
+
+
+        static int CompareStringByLength(string str1, string str2)
+        {
+            if (str1.Length > str2.Length)
+                return 1;
+            else if (str1.Length < str2.Length)
+                return -1;
+            else
+            {
+                return (str1[0] - str2[0]);
+            }
+        }
+
+
+        static int CompareInt(int val1, int val2)
+        {
+            if (val1 > val2)
+                return 1;
+            else return -1;
         }
     }
 }
